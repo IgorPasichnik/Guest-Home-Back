cc528d453fb3ba5166aad7e689872d01bcb7fce5
IgorPasichnik <nekto.igoryambus@gmail.com>
2025-03-20 03:43:31 +0300

    edit options2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-app.use(express.json()); // <-- важно!
+app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
